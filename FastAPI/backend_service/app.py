80c9518ac5a25a6dec599eb80106ffbecc0eb368
dark-0ne <17182126+dark-0ne@users.noreply.github.com>
2023-09-11 17:49:29 +0300

    Add CORS to FastAPI
    Simple CORS middleware was added to FastAPI app to open CORS on all
    methods and origins (very insecure).
    
    TODO: Upgrade CORS for better security

--- FastAPI/backend_service/app.py
+++ FastAPI/backend_service/app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
@@ -8,9 +10,17 @@ from backend_service.models.user import User
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 async def app_init():
     """Initialize application services"""
     app.db = AsyncIOMotorClient(CONFIG.mongo_uri).account
-    await init_beanie(app.db, document_models=[User])
+    await init_beanie(app.db, document_models=[User]
