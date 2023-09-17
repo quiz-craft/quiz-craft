be9f0c86e418b097baa5fd10282d0921bcaf6e91
Mehrdad Kaheh <kahe.mehrdad@gmail.com>
2023-09-17 20:22:43 +0300

    done #28 - handle user token if server returns 401

--- client/quiz-craft/src/services/http/axiosClient.js
+++ client/quiz-craft/src/services/http/axiosClient.js
@@ -1,26 +1,42 @@
 import { createAxiosClient } from "./createAxiosClient";
 import { baseUrl } from "../Settings";
-import {getToken} from "../AuthStoreService"
+import {getToken, removeToken} from "../AuthStoreService"
 
+const REFRESH_TOKEN_URL = ''
 const BASE_URL = baseUrl;
 
 function getCurrentAccessToken() {
+    // this is how you access the zustand store outside of React.
     return getToken();
 }
 
+function getCurrentRefreshToken() {
+    // this is how you access the zustand store outside of React.
+    //return useAuthStore.getState().refreshToken
+    return null;
+}
+
+
+function setRefreshedTokens(tokens){
+    console.log('set tokens...')
+}
+
 async function logout(){
-    // TODO
     console.log('logout...')
+    removeToken();
 }
 
 export const client = createAxiosClient({
     options: {
         baseURL: BASE_URL,
-        timeout: 3000,
+        timeout: 300000,
         headers: {
             'Content-Type': 'application/json',
         }
     },
     getCurrentAccessToken,
-    logout
+    getCurrentRefreshToken,
+    refreshTokenUrl: REFRESH_TOKEN_URL,
+    logout,
+    setRefreshedTokens
 })
--- client/quiz-craft/src/services/http/createAxiosClient.js
+++ client/quiz-craft/src/services/http/createAxiosClient.js
@@ -1,8 +1,28 @@
-import axios from 'axios';
+//reference: https://dev.to/mihaiandrei97/jwt-authentication-using-axios-interceptors-55be
+import axios from "axios";
+
+let failedQueue = [];
+let isRefreshing = false;
+
+const processQueue = (error) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve();
+    }
+  });
+
+  failedQueue = [];
+};
 
 export function createAxiosClient({
   options,
   getCurrentAccessToken,
+  getCurrentRefreshToken,
+  refreshTokenUrl,
+  logout,
+  setRefreshedTokens,
 }) {
   const client = axios.create(options);
 
@@ -21,6 +41,88 @@ export function createAxiosClient({
     }
   );
 
- return client;
+  client.interceptors.response.use(
+    (response) => {
+      console.log("client.interceptors.response is ok")
+      // Any status code that lie within the range of 2xx cause this function to trigger
+      // Do something with response data
+      return response;
+    },
+    (error) => {
+      console.log("client.interceptors.response faced error")
+      const originalRequest = error.config;
+      // In "axios": "^1.1.3" there is an issue with headers, and this is the workaround.
+      originalRequest.headers = JSON.parse(
+        JSON.stringify(originalRequest.headers || {})
+      );
+      const refreshToken = getCurrentRefreshToken();
+      // If error, process all the requests in the queue and logout the user.
+      const handleError = (error) => {
+        processQueue(error);
+        logout();
+        return Promise.reject(error);
+      };
 
+      // TODO: temporary just to log out user without referesh token. In the future 
+      // it should be replaced with further if condition "Refresh token conditions"
+      if(error.response?.status === 401){
+        logout()
+      }
+
+      // Refresh token conditions
+      if (
+        refreshToken &&
+        error.response?.status === 401 &&
+        error.response.data.message === "TokenExpiredError" &&
+        originalRequest?.url !== refreshTokenUrl &&
+        originalRequest?._retry !== true
+      ) {
+
+        if (isRefreshing) {
+          return new Promise(function (resolve, reject) {
+            failedQueue.push({ resolve, reject });
+          })
+            .then(() => {
+              return client(originalRequest);
+            })
+            .catch((err) => {
+              return Promise.reject(err);
+            });
+        }
+        isRefreshing = true;
+        originalRequest._retry = true;
+        return client
+          .post(refreshTokenUrl, {
+            refreshToken: refreshToken,
+          })
+          .then((res) => {
+            const tokens = {
+              accessToken: res.data?.accessToken,
+              refreshToken: res.data?.refreshToken,
+            };
+            setRefreshedTokens(tokens);
+            processQueue(null);
+
+            return client(originalRequest);
+          }, handleError)
+          .finally(() => {
+            isRefreshing = false;
+          });
+      }
+
+      // Refresh token missing or expired => logout user...
+      if (
+        error.response?.status === 401 &&
+        error.response?.data?.message === "TokenExpiredError"
+      ) {
+        return handleError(error);
+      }
+
+      // Any status codes that falls outside the range of 2xx cause this function to trigger
+      // Do something with response error
+      return Promise.reject(error);
+    }
+  );
+
+  return client;
 }
