a653b0ac723e728b9ea62290cb0d0457d63f36ad
dark-0ne <17182126+dark-0ne@users.noreply.github.com>
2023-09-19 15:28:53 +0300

    Add basic Kubernetes configuration
    A simple K8s config has been added. Current config relies on
    Mongodb Kubernetes Operator
    (https://github.com/mongodb/mongodb-kubernetes-operator) for setting up
    MongoDB on K8s cluster and External Secrets Operator
    (https://github.com/external-secrets/external-secrets) for secret
    managing using GCP Secret Manager.
    
    Close #22

--- FastAPI/backend_service/config.py
+++ FastAPI/backend_service/config.py
@@ -4,6 +4,7 @@ FastAPI server configuration
 
 # pylint: disable=too-few-public-methods
 from datetime import timedelta
+import os
 
 from decouple import config
 from pydantic import BaseModel
@@ -15,10 +16,12 @@ class Settings(BaseModel):
     root_url: str = config("ROOT_URL")
 
     # Mongo Engine settings
-    mongo_uri: str = config("MONGO_URI")
+    #mongo_uri: str = config("MONGO_URI")
+    #mongo_uri: str = config("MONGO_URI")
+    mongo_uri: str = os.environ['MONGO_URI']
 
     # Security settings
-    jwt_secret_key: str = config("SECRET_KEY")
+    jwt_secret_key: str = os.environ['JWT_SECRET_KEY']
     jwt_exipiration_time: timedelta = config(
         "JWT_EXPIRATION_MINUTES", default=15,
         cast=lambda m: timedelta(minutes=int(m)))
@@ -26,8 +29,8 @@ class Settings(BaseModel):
     # FastMail SMTP server settings
     mail_server: str = config("MAIL_SERVER", default="mail.quiz-craft.com")
     mail_port: int = config("MAIL_PORT", default=587, cast=int)
-    mail_username: str = config("MAIL_USERNAME", default="")
-    mail_password: str = config("MAIL_PASSWORD", default="")
+    mail_username: str = os.environ['MAIL_SERVER_USER']
+    mail_password: str = os.environ['MAIL_SERVER_PASSWORD']
     mail_sender: str = config("MAIL_SENDER", default="noreply@quiz-craft.com")
 
     testing: bool = config("TESTING", default=False, cast=bool)
