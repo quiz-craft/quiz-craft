8ad2375e17c6b2b1bd046f935ad62ad9908e4a76
dark-0ne <17182126+dark-0ne@users.noreply.github.com>
2023-09-20 17:08:02 +0300

    Add ConfigMap for FastAPI decouple module
    Added a new K8s ConfigMap that takes care of non-secret configuration
    required by FastAPI.

--- FastAPI/backend_service/config.py
+++ FastAPI/backend_service/config.py
@@ -16,12 +16,10 @@ class Settings(BaseModel):
     root_url: str = config("ROOT_URL")
 
     # Mongo Engine settings
-    #mongo_uri: str = config("MONGO_URI")
-    #mongo_uri: str = config("MONGO_URI")
-    mongo_uri: str = os.environ['MONGO_URI']
+    mongo_uri: str = config("MONGO_URI")
 
     # Security settings
-    jwt_secret_key: str = os.environ['JWT_SECRET_KEY']
+    jwt_secret_key: str = config("JWT_SECRET_KEY")
     jwt_exipiration_time: timedelta = config(
         "JWT_EXPIRATION_MINUTES", default=15,
         cast=lambda m: timedelta(minutes=int(m)))
@@ -29,8 +27,8 @@ class Settings(BaseModel):
     # FastMail SMTP server settings
     mail_server: str = config("MAIL_SERVER", default="mail.quiz-craft.com")
     mail_port: int = config("MAIL_PORT", default=587, cast=int)
-    mail_username: str = os.environ['MAIL_SERVER_USER']
-    mail_password: str = os.environ['MAIL_SERVER_PASSWORD']
+    mail_username: str = config("MAIL_SERVER_USER")
+    mail_password: str = config("MAIL_SERVER_PASSWORD")
     mail_sender: str = config("MAIL_SENDER", default="noreply@quiz-craft.com")
 
     testing: bool = config("TESTING", default=False, cast=bool)
